19d73cc021ef5ffca77b0dc4074427ca479fceb4
Serhii Moroz <sergiy.m.lutsk@gmail.com>
2022-09-09 23:49:15 +0300

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const refIngredients = document.querySelector("#ingredients");
+const elements = ingredients.map((element) => {
+  const liEl = document.createElement("li");
+  liEl.classList.add("item");
+  liEl.textContent = element;
+  return liEl;
+});
+refIngredients.append(...elements);
