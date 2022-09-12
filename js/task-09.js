7e42c95dcf2d76d2305a535dfd828bd7ec3d77a6
Serhii Moroz <sergiy.m.lutsk@gmail.com>
2022-09-12 14:30:34 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
+const refBody = document.querySelector("body");
+const refBtnChange = document.querySelector(".change-color");
+const refSpan = document.querySelector(".color");
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+refBtnChange.addEventListener("click", (event) => {
+  refBody.style.backgroundColor = getRandomHexColor();
+  refSpan.innerHTML = getRandomHexColor();
+});
